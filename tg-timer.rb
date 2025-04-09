52ca7a1550805a2df18261fc2cbb9c520f41250c
PetitLuc38 <78622151+PetitLuc38@users.noreply.github.com>
2025-04-09 17:59:05 +0200

    Update tg-timer.rb

--- tg-timer.rb
+++ tg-timer.rb
@@ -2,7 +2,7 @@ class TgTimer < Formula
   desc "A program for timing mechanical watches"
   homepage "https://tg.ciovil.li"
   url "https://github.com/PetitLuc38/tg-XYZY42-6.1/archive/refs/tags/v0.6.1-tpiepho.tar.gz"
-  sha256 "c88889bd391212d9104bca9918fb573a4ff96a752717b4a779d33f637c633ce7"
+  sha256 "3e13d22f42d6565fcf48b3411ea1f7e7bfb97e05b26b418a69efb80430eb9e66"
   head "https://github.com/PetitLuc38/tg-XYZY42-6.1.git", :branch => "master"
 
   depends_on "pkg-config" => :build
