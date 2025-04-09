0d85507202c7b087a97b42505f9fb5cddfd5fe4b
PetitLuc38 <78622151+PetitLuc38@users.noreply.github.com>
2025-04-09 17:48:03 +0200

    Update tg-timer.rb

--- tg-timer.rb
+++ tg-timer.rb
@@ -1,9 +1,9 @@
 class TgTimer < Formula
   desc "A program for timing mechanical watches"
   homepage "https://tg.ciovil.li"
-  url "https://github.com/vacaboja/tg/archive/v0.5.0.tar.gz"
+  url "https://github.com/PetitLuc38/tg-XYZY42-6.1/archive/refs/tags/v0.6.1-tpiepho.tar.gz"
   sha256 "c88889bd391212d9104bca9918fb573a4ff96a752717b4a779d33f637c633ce7"
-  head "https://github.com/vacaboja/tg.git", :branch => "master"
+  head "https://github.com/PetitLuc38/tg-XYZY42-6.1.git", :branch => "master"
 
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
@@ -20,8 +20,8 @@ class TgTimer < Formula
     system "./autogen.sh"
     system "./configure"
     system "make"
-    bin.install "tg-timer"
-    man1.install "docs/tg-timer.1"
+    bin.install "tg-timer-6.1"
+    man1.install "docs/tg-timer-6.1.1"
   end
 
   test do
